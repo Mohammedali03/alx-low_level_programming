284226d52f549c200d0595f5971d394149efed5e
Mohammedali03 <elidrissi.mohammedali07@gmail.com>
2023-09-12 16:14:19 -0700

    0

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,19 +9,22 @@
  */
 int print_sign(int n)
 {
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	}
-	else if (n == 0)
-	{
-		_putchar('0'); // Changed 48 to '0' for readability
-		return (0);
-	}
-	else if (n < 0)
-	{
-		_putchar('-');
-		return (-1);
-	}
+    if (n > 0)
+    {
+        _putchar('+');
+        return (1);
+    }
+    else if (n == 0)
+    {
+        _putchar('0');
+        return (0);
+    }
+    else if (n < 0)
+    {
+        _putchar('-');
+        return (-1);
+    }
+    
+    // Default return statement (added to handle all cases)
+    return (0);
 }
