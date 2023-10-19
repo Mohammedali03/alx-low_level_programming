f46bfd94adc98e2509912939ce4b73544a2ae743
Mohammedali03 <elidrissi.mohammedali07@gmail.com>
2023-10-19 13:36:05 -0700

    0

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+void print_message(void) __attribute__((constructor));
+
+/**
+ * print_message - Print specified message before the main
+ */
+void print_message(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
