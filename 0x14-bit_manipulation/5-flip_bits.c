c6eafb8a5691793688353b6c8aee14299d240d70
Mohammedali03 <elidrissi.mohammedali07@gmail.com>
2023-10-26 12:13:57 -0700

    0

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,35 +1,25 @@
 #include "main.h"
 
 /**
- * count_set_bits - set bits in the XOR result
- * @n: result of XOR
- * Return: count
- */
-
-
-int count_set_bits(int n)
-{
-	int count = 0;
-
-	while (n > 0)
-	{
-		count++;
-		n &= (n - 1);
-	}
-	return (count);
-}
-
-
-/**
- * flip_bits - returns the number of bits you would need
- * to flip to get from one number to other one.
+ * flip_bits - counts the number of bits to change
+ * to get from one number to other one
  * @n: the first number
- * @m: thr second number
- * Return: the number of bits to flip
+ * @m: the second number
+ *
+ * Return: number of bits to change
  */
-
-
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	return (count_set_bits(n ^ m));
+	int i, count = 0;
+	unsigned long int current;
+	unsigned long int exclusive = n ^ m;
+
+	for (i = 63; i >= 0; i--)
+	{
+		current = exclusive >> i;
+		if (current & 1)
+			count++;
+	}
+
+	return (count);
 }
