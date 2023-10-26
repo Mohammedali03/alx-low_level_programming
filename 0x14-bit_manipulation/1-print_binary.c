2752f2486aadfee6e64ea1fa1a0780df80d037c3
Mohammedali03 <elidrissi.mohammedali07@gmail.com>
2023-10-26 12:11:43 -0700

    0

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,45 +1,26 @@
 #include "main.h"
 
 /**
- * checkLen - checks the binary length of an integer
- * @n: the integer
- * Return: the length of the intger
+ * print_binary - prints the binary equivalent of a decimal number
+ * @n: the number to print in binary
  */
-
-int checkLen(unsigned long int n)
-{
-	int i = 0;
-
-	while (n)
-	{
-		i++;
-		n >>= 1;
-	}
-	return (i);
-}
-
-/**
- * print_binary -  prints the binary representation of a number.
- * @n: the number
- */
-
-
 void print_binary(unsigned long int n)
 {
-	int i;
+	int i, count = 0;
+	unsigned long int current;
 
-	if (n == 0)
-		_putchar('0');
-
-	for (i = checkLen(n) - 1; i >= 0; i--)
+	for (i = 63; i >= 0; i--)
 	{
-		if (n & (1 << i))
+		current = n >> i;
+
+		if (current & 1)
 		{
 			_putchar('1');
+			count++;
 		}
-		else
-		{
+		else if (count)
 			_putchar('0');
-		}
 	}
+	if (!count)
+		_putchar('0');
 }
