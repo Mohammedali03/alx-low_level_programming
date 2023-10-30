1e4c4dcdb79c816f86d05fa1ab4239283a7907ce
Mohammedali03 <elidrissi.mohammedali07@gmail.com>
2023-10-30 14:58:31 -0700

    0

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,17 +3,23 @@
 /**
  * create_file -  creates a file.
  *
- * @filename: the name of the file to create
- * @text_content: is a NULL terminated string to write in the file
+ * @filename: is the name of the file to create
+ * @text_content: is a NULL terminated string to write to the file
  * Return: 1 on success, -1 on failure
  */
 
 int create_file(const char *filename, char *text_content)
 {
-	int file, flag;
+	int file, flag, len = 0;
 
 	if (filename == NULL)
 		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
 	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	flag = write(file, text_content, strlen(text_content));
